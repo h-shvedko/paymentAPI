1e111fbc735425742f02888dd633d0248f0ef99b
Hennadii Shvedko <hennadii.shvedko@jagaad.com>
2023-10-05 16:41:46 +0200

    Added new endpoint

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 use PaymentApi\Middleware\CustomErrorHandler;
 use Slim\Factory\AppFactory;
 use Slim\Routing\RouteCollectorProxy;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../container/container.php';
@@ -10,6 +12,10 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->safeLoad();
 //APP_ROOT
 $app = AppFactory::createFromContainer(container: $container);
+
+$app->get('/v1', function (Request $request, Response $response, $args) {
+    echo "Hello V1!";
+});
 
 //Methods
 $app->group('/v1/methods', function (RouteCollectorProxy $group) {
